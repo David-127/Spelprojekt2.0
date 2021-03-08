6f3381a953428d67655c62e134dcd94dee5e24dc
David-127 <71015265+David-127@users.noreply.github.com>
2021-03-09 00:00:28 +0100

    det fungerar typ

--- main.js
+++ main.js
@@ -45,6 +45,7 @@ let lastcolor;
 let lastruta;
 let valbarRutor=[];
 let lastId;
+var kill = false;
 
 
 
@@ -53,64 +54,321 @@ function flytta(obj){
     var bricka = obj.id;
     var ruta = obj.parentNode.id;
     var färg = obj.classList[0];
+
         if(färg === "brickaröd"){
             färg = "röd";
-            let valbara=[];
             var splita = bricka.split("-");
             var rad = parseInt(splita[0]);
             var column = parseInt(splita[1]);
             var temp = String(rad-1)+"-"+String(column+1); 
-            var temp2 = String(rad-1)+"-"+String(column-1); 
-            var element = document.getElementById("ruta"+temp);
-            element.classList.toggle('valbar');
-            var element2 = document.getElementById("ruta"+temp2);
-            element2.classList.toggle('valbar');
-            lastcolor ="röd";
-            lastruta = ruta;
-            valbarRutor[0]= element;
-            valbarRutor[1]=element2;
-            if(element.innerHTML!=""){
-                console.log(element.childNodes[0].classList[0]);
-                console.log(obj.classList[0]);
-                if(element.childNodes[0].classList[0] != obj.classList[0]){
-                    
-                }
+            var temp2 = String(rad-1)+"-"+String(column-1);
 
-            }      
+	    lastcolor ="röd";
+            lastruta = ruta;
+
+
+	    if (column != 8){
+		var element = document.getElementById("ruta"+temp);
+		element.classList.add('valbar');
+	    }
+
+	    if (column != 1) {			    
+		var element2 = document.getElementById("ruta"+temp2);
+		element2.classList.add('valbar');
+	    }
+
+            
+	    if (column != 8) {
+		valbarRutor[0] = element;
+	    } else {
+		// lastruta.classList.toggle('valbar');
+		valbarRutor[0] = " ";
+	    }
+
+	    if (column != 1) {
+		valbarRutor[1]= element2;
+	    } else {
+		valbarRutor[1] = " ";
+	    }
+	    
+            if(valbarRutor[0].innerHTML!="" && valbarRutor[0] != " "){
+
+		if(valbarRutor[0].childNodes[0].classList[0] != obj.classList[0]){
+
+
+		    valbarRutor[0].classList.add('valbar');
+
+		    var element_id = valbarRutor[0].id.split("a").pop();
+
+		    rad = element_id.split("-")[0];
+		    column = element_id.split("-")[1];
+
+		    column = parseInt(column) + 1;
+		    rad = parseInt(rad) - 1;
+
+		    
+		    var högerId = "ruta" + rad + "-" + column;
+		    var höger = document.getElementById(högerId);
+		    
+		    if (element.classList.contains(obj.classList[0])) {
+			console.log("samma färg");
+		    }
+		    
+		    if (höger != null || höger != undefined) {
+
+		    
+		    if (höger.hasChildNodes()) {
+
+			höger.classList.remove('valbar');
+
+		    } else {
+
+			höger.classList.add('valbar');
+		    }
+
+			höger.addEventListener('click',function() { valbarRutor[0].innerHTML = "";
+								    valbarRutor[0] = "";
+								  });
+
+			console.log(höger);
+			console.log(valbarRutor[0]);
+			// höger.addEventListener('click', eventListen(0));
+		    }
+ 		    valbarRutor[0].classList.remove('valbar');
+
+
+                } 
+
+
+		
+            } if (valbarRutor[1].innerHTML != "" && valbarRutor[1] != " ") {
+
+		if(valbarRutor[1].childNodes[0].classList[0] != obj.classList[0]){
+
+		    console.log("röd vänster");
+		    
+		    valbarRutor[1].classList.add('valbar');
+		    var element2_id = valbarRutor[1].id.split("a").pop();
+
+		    rad = element2_id.split("-")[0];
+		    column = element2_id.split("-")[1];
+
+		    
+		    
+		    column = parseInt(column)- 1;
+		    rad = parseInt(rad) - 1;
+
+		    
+		    var vänsterId = "ruta" + rad + "-" + column;
+		    
+		    var vänster = document.getElementById(vänsterId);
+
+		    if (vänster != null || vänster != undefined) {
+			
+		    
+			if (vänster.hasChildNodes()) {
+			    vänster.classList.remove('valbar');
+
+			} else {
+			    vänster.classList.add('valbar');
+			}
+			valbarRutor[1].classList.remove('valbar');
+
+			vänster.addEventListener('click',function() { valbarRutor[1].innerHTML = "";
+								      valbarRutor[1] = "";
+								    });
+			console.log(vänster);
+			console.log(valbarRutor[0]);
+			// vänster.addEventListener('click', eventListen(1));
+			
+		    }
+		    // vänster.classList.add('valbar');
+		    valbarRutor[1].classList.remove('valbar');
+		    
+		}
+		
+	    }
+	    
         }
-        else if(färg === "brickasvart"){
-             färg="svart";
-             var splita = bricka.split("-");
-             var rad = parseInt(splita[0]);
-             var column = parseInt(splita[1]);
-             var temp = String(rad+1)+"-"+String(column+1); 
-             var temp2 = String(rad+1)+"-"+String(column-1); 
-             var element = document.getElementById("ruta"+temp);
-             element.classList.toggle('valbar');
-             var element2 = document.getElementById("ruta"+temp2);
-             element2.classList.toggle('valbar');
-             lastcolor = "svart";
-             lastruta = ruta;
-             valbarRutor[0]= element;
-             valbarRutor[1]=element2;
-        }
+         if(färg === "brickasvart"){
+            färg="svart";
+            var splita = bricka.split("-");
+            var rad = parseInt(splita[0]);
+            var column = parseInt(splita[1]);
+            var temp = String(rad+1)+"-"+String(column+1); 
+            var temp2 = String(rad+1)+"-"+String(column-1); 
+
+            lastcolor = "svart";
+            lastruta = ruta;
+
+
+	     	    
+	    if (column != 8){
+		var element = document.getElementById("ruta"+temp);
+		element.classList.add('valbar');
+	    }
+
+	    if (column != 1) {			    
+		var element2 = document.getElementById("ruta"+temp2);
+		element2.classList.add('valbar');
+	    }
+
+            
+	    if (column != 8) {
+		valbarRutor[0] = element;
+
+	    } else {
+		// lastruta.classList.toggle('valbar');
+		valbarRutor[0] = " ";
+		console.log(lastruta);
+	    }
+
+	    if (column != 1) {
+		valbarRutor[1]= element2;
+	    } else {
+		valbarRutor[1] = " ";
+	    }
+
+	     
+             if(valbarRutor[0].innerHTML!="" && valbarRutor[0] != " "){
+		 console.log("höger svart");
+		 if(valbarRutor[0].childNodes[0].classList[0] != obj.classList[0]){
+
+		     valbarRutor[0].classList.add('valbar');
+		    
+		     var element_id = valbarRutor[0].id.split("a").pop();
+		     
+		     rad = element_id.split("-")[0];
+		     column = element_id.split("-")[1];
+
+		     column = parseInt(column)+ 1;
+		     rad = parseInt(rad) +1;
+
+
+		     var högerId = "ruta" + rad + "-" + column;
+		     var höger = document.getElementById(högerId);
+
+		     if (höger != null || höger != undefined) {
+
+			 
+			 if (höger.hasChildNodes()) {
+			     höger.classList.remove('valbar');
+			
+			 } else {
+			     höger.classList.add('valbar');
+			 }
+			 höger.addEventListener('click',function() {
+			     valbarRutor[0].innerHTML = "";});
+			 // höger.addEventListener('click', eventListen(0));
+                     }
+		     
+		     valbarRutor[0].classList.remove('valbar');
+		 }
+		 
+	     } if (valbarRutor[1].innerHTML != "" && valbarRutor[1] != " ") {
+
+		     if(valbarRutor[1].childNodes[0].classList[0] != obj.classList[0]){
+			 console.log("vänster svart");
+			 valbarRutor[1].classList.add('valbar');
+			 var element2_id = valbarRutor[1].id.split("a").pop();
+			 
+			 rad = element2_id.split("-")[0];
+			 column = element2_id.split("-")[1];
+			 
+			 column = parseInt(column) - 1;
+			 rad = parseInt(rad) + 1;
+
+			 var vänsterId = "ruta" + rad + "-" + column;
+			 var vänster = document.getElementById(vänsterId);
+
+
+			if (vänster != null || vänster != undefined) {
+
+			    if (vänster.hasChildNodes()) {
+			    
+				console.log("vänster svart has child");
+				vänster.classList.remove('valbar');
+				
+			    } else{
+				vänster.classList.add('valbar');
+			    }
+
+			    vänster.addEventListener('click',function() { valbarRutor[1].innerHTML = "";
+									  valbarRutor[1] = "";
+									});
+			    // vänster.addEventListener('click', eventListen(1));
+			}
+			valbarRutor[1].classList.remove('valbar');
+			
+			
+		    }
+		
+		}
+	     
+             }
        
-}
-const rutor = document.querySelectorAll('.ruta-svart');
+	 }
+
+
+
+const rutor = document.querySelectorAll('.ruta-svart , .ruta-röd');
+
+// console.log(rutor);
 rutor.forEach(ruta =>ruta.addEventListener("click",flytta2));
 function flytta2(){
     if(this.classList.contains('valbar')){
         if(lastcolor == "röd"){
-         document.getElementById(lastruta).innerHTML="";
-         valbarRutor[0].classList.toggle('valbar');
-         valbarRutor[1].classList.toggle('valbar');
-        var plats = this.id.split('a').pop();
-        this.innerHTML= '<div class="brickaröd" id="'+plats+'" onclick = "flytta(this)"></div>';
+	    document.getElementById(lastruta).innerHTML="";		
+
+	    if (valbarRutor[0] != " ") {
+		// valbarRutor[0].classList.toggle('valbar');
+		rutor.forEach(ruta => ruta.classList.remove('valbar'));
+
+
+		    rutor.forEach(ruta => ruta.removeEventListener('click', eventListen));
+
+		    // rutor.forEach(ruta =>ruta.removeEventListener("click",flytta2));
+
+
+	    }
+
+	    if (valbarRutor[1] != " ") {
+		// valbarRutor[1].classList.toggle('valbar');
+		rutor.forEach(ruta => ruta.classList.remove('valbar'));
+
+
+		    rutor.forEach(ruta => ruta.removeEventListener('click', eventListen));
+
+
+	    }
+
+
+            var plats = this.id.split('a').pop();	   
+            this.innerHTML= '<div class="brickaröd" id="'+plats+'" onclick = "flytta(this)"></div>';
+
         }
         else if(lastcolor == "svart"){
             document.getElementById(lastruta).innerHTML="";
-            valbarRutor[0].classList.toggle('valbar');
-            valbarRutor[1].classList.toggle('valbar');
+
+	    if (valbarRutor[0] != " ") {
+		rutor.forEach(ruta => ruta.classList.remove('valbar'));
+
+		if (kill == true) {
+		    rutor.forEach(ruta => ruta.removeEventListener('click', eventListen));
+		    kill = false;
+		}
+	    }
+
+	    if (valbarRutor[1] != " ") {
+		rutor.forEach(ruta => ruta.classList.remove('valbar'));
+
+		if (kill == true) {
+		    rutor.forEach(ruta => ruta.removeEventListener('click', eventListen));
+		    kill = false;
+		}
+	    }
+
+
             var plats = this.id.split('a').pop();
             this.innerHTML= '<div class="brickasvart" id="'+plats+'" onclick = "flytta(this)"></div>';
         }
@@ -120,5 +378,23 @@ function flytta2(){
 
 
 
+function eventListen(num){
+
+
+    if (kill == true) {
+
+	if (num == 0) {
+	    console.log(valbarRutor[0]);
+	    valbarRutor[0].innerHTML ="";
+	} else {
+	    console.log(valbarRutor[1]);
+	    valbarRutor[1].innerHTML ="";
+	}
+	kill = false;
+	
+    }
+    kill = true;
+    
+}
 
 
